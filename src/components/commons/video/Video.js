b5ff98a8c68a653ddf249b1e796e702ba8fa8769
Natalia Barco <anbarco@unicauca.edu.co>
2023-03-28 02:05:11 -0500

    Buttons to show each chapter, chapter 1 and chapter2 added

--- src/components/commons/video/Video.js
+++ src/components/commons/video/Video.js
@@ -5,22 +5,32 @@ import videoPath2 from '../../../../src/capitulo2.mp4';
 
 const Video = () => {
   const [videoUrl, setVideoUrl] = useState(videoPath1);
+  const chapters = [
+    { name: 'Capítulo 1', url: videoPath1 },
+    { name: 'Capítulo 2', url: videoPath2 },
+  ];
 
-  const handleButtonClick = () => {
-    setVideoUrl(videoPath2);
+  const handleChapterClick = (chapterUrl) => {
+    setVideoUrl(chapterUrl);
   };
 
   return (
     <div className="player-wrapper">
-      <ReactPlayer  
-        className='react-player'
+      <ReactPlayer
+        className="react-player"
         controls
         url={videoUrl}
-        width='100%'
-        height='90%'
+        width="100%"
+        height="90%"
       />
-      <button onClick={handleButtonClick}>Cambiar video</button>
-    </div> 
+      <div className="chapter-buttons">
+        {chapters.map((chapter) => (
+          <button key={chapter.name} onClick={() => handleChapterClick(chapter.url)}>
+            {chapter.name}
+          </button>
+        ))}
+      </div>
+    </div>
   );
 };
 
