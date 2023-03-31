54767356bd7f61c5fe9fcde14e19a039b5a86fb4
Natalia Barco <anbarco@unicauca.edu.co>
2023-03-31 02:50:19 -0500

    Main logo added, hover sound in buttons added

--- src/components/commons/video/Video.js
+++ src/components/commons/video/Video.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactPlayer from 'react-player';
 import intropath from '../../../videos/introvideo.mp4'
 import videoPath1 from '../../../videos/capitulo1.mp4';
@@ -6,9 +6,9 @@ import videoPath2 from '../../../videos/capitulo2.mp4';
 import videoPath3 from '../../../videos/capitulo3.mp4';
 import videoPath4 from '../../../videos/capitulo4.mp4';
 import videoPath5 from '../../../videos/capitulo5.mp4';
-
 import './Video.css'
 
+const sonidoPath = `${process.env.PUBLIC_URL}/audiohover.mp3`;
 const Video = () => {
   const [videoUrl, setVideoUrl] = useState(intropath);
   const [showImages, setShowImages] = useState(true);
@@ -18,8 +18,11 @@ const Video = () => {
     { name: 'Líderes sociales II', url: videoPath3, image: './cap3.png' },
     { name: 'Construcción de barriosI', url: videoPath4, image: './cap4.png' },
     { name: 'Mujeres lucha y esperanza', url: videoPath5, image: './cap5.png' },
+    { name: 'logo centro', url:videoPath1 , image: './Logo200px.png' },
     { name: 'Intro', style: {display: 'none', top: '0px', left: '0px'} },
   ];
+
+  const audioRef = useRef(null);
 
   const handleChapterClick = (chapterUrl) => {
     setShowImages(false);
@@ -29,7 +32,7 @@ const Video = () => {
   const handleVideoEnded = () => {
     setShowImages(true);
   }
-
+  
   return (
     <div className="player-wrapper">
       <ReactPlayer
@@ -50,9 +53,12 @@ const Video = () => {
             alt={chapter.name}
             style={chapter.style}
             onClick={() => handleChapterClick(chapter.url)}
+            onMouseEnter={() => audioRef.current.play()}
+            onMouseLeave={() => audioRef.current.pause()}
           />
         ))}
       </div>
+      <audio ref={audioRef} src={sonidoPath} />
     </div>
   );
 };
