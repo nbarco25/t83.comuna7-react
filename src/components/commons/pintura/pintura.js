074c52fd95adfdb84bba18c513a3b36a678f572e
Natalia Barco <anbarco@unicauca.edu.co>
2023-07-27 17:42:44 -0500

    Add pinturas's  images and pinturas's gallery first version

--- src/components/commons/pintura/pintura.js
+++ src/components/commons/pintura/pintura.js
@@ -1,3 +1,40 @@
-export default function Pintura() {
-    // código del componente
-}
+import React from 'react';
+import './Pintura.css';
+import ModalImage from 'react-modal-image';
+
+// Importa las imágenes desde tu carpeta de imágenes
+import img1 from '../../../img/pinturas_jpg/image00002.jpeg';
+import img2 from '../../../img/pinturas_jpg/image00003.jpeg';
+import img3 from '../../../img/pinturas_jpg/image00004.jpeg';
+import img4 from '../../../img/pinturas_jpg/image00005.jpeg';
+import img5 from '../../../img/pinturas_jpg/image00006.jpeg';
+import img6 from '../../../img/pinturas_jpg/image00007.jpeg';
+import img7 from '../../../img/pinturas_jpg/image00008.jpeg';
+import img8 from '../../../img/pinturas_jpg/image00009.jpeg';
+import img9 from '../../../img/pinturas_jpg/image00010.jpeg';
+
+// Importa más imágenes aquí
+
+const Pintura = () => {
+    // Podrías reemplazar este array con tus propias imágenes
+    const imagenes = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
+
+    return (
+        <div className="pintura">
+            <h1>Galería de Pinturas</h1>
+            <div className="imagenes">
+                {imagenes.map((imagen, index) => (
+                    <ModalImage
+                        key={index}
+                        small={imagen}
+                        large={imagen}
+                        alt={`Imágen ${index + 1}`}
+                        className="image-small"
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Pintura;
--- src/components/commons/podcast/Podcast.js
+++ src/components/commons/podcast/Podcast.js
@@ -1,3 +1,37 @@
-export default function Podcast() {
-    // código del componente
-}
+import React from 'react';
+import './Podcast.css';
+
+const Podcast = () => {
+    // Podrías reemplazar este array con la información de tus podcasts
+    const podcasts = [
+        {
+            title: 'Capítulo 1',
+            url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1593625207&color=%23d2a944&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+        },
+        // Agrega más objetos de podcasts aquí
+    ];
+
+    return (
+        <div className="podcast-container">
+            <h1>Podcast T83Comuna7</h1>
+            {podcasts.map((podcast, index) => (
+                <div className="podcast-chapter" key={index}>
+                    <h2>{podcast.title}</h2>
+                    <div className="soundcloud-embed">
+                        <iframe 
+                            title={podcast.title}
+                            width="100%" 
+                            height="300" 
+                            scrolling="no" 
+                            frameborder="no" 
+                            allow="autoplay" 
+                            src={podcast.url}
+                        />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Podcast;
