4b7009e31b3babcd636c6fe0f61d0db9f7b8c658
Natalia Barco <anbarco@unicauca.edu.co>
2024-02-19 22:50:10 -0500

    galeria de pinturas modificada

--- src/components/commons/pintura/pintura.js
+++ src/components/commons/pintura/pintura.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './pintura.css';
 import ModalImage from 'react-modal-image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 function importAll(r) {
     let images = {};
@@ -13,22 +16,46 @@ const images = importAll(require.context('../../../img/pinturas_jpg', false, /\.
 const Pintura = () => {
     // Utiliza las imágenes importadas
     const imagenes = Object.values(images);
+    const [currentIndex, setCurrentIndex] = useState(0);
     console.log(imagenes);
+
+    const previousImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? imagenes.length - 1 : prevIndex - 1));
+    };
+
+    const nextImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === imagenes.length - 1 ? 0 : prevIndex + 1));
+    };
+
+
     return (
+
         <div className="pintura">
-            <h1>Galería de Pinturas</h1>
-            <div className="imagenes">
-                {imagenes.map((imagen, index) => (
-                    <ModalImage
-                        key={index}
-                        small={imagen}
-                        large={imagen}
-                        alt={`Imágen ${index + 1}`}
-                        className="image-small"
+            <div className="imagen-columna">
+                <br/>
+                <div className="carousel-container">
+                    <button className="carousel-button" onClick={previousImage}>
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </button>
+                    <img
+                        src={imagenes[currentIndex]}
+                        alt={`Imagen ${currentIndex + 1}`}
+                        className="carousel-image"
                     />
-                ))}
+                    <button className="carousel-button" onClick={nextImage}>
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </button>
+                </div>
             </div>
+           
         </div>
+
+
+
+
+
+
+
     );
 };
 
